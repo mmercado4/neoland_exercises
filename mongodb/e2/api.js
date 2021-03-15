79268f5d6b42e573a1bb2fef965273445c45262c
Miguel Mercado <miguel.mercadosalgado@gmail.com>
2021-03-15 18:47:46 +0100

    GET ONE and PUT methods

--- mongodb/e2/api.js
+++ mongodb/e2/api.js
@@ -11,7 +11,6 @@ api.use(bodyParser.json());
 api.use(bodyParser.urlencoded({ extended: true })); //Decodificamos la info del body.
 
 //CORS Policy
-
 api.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
@@ -29,7 +28,6 @@ api.use((req, res, next) => {
 });
 
 //DB Conection
-
 mongoose.connect(
   "mongodb://localhost/pokemon",
   { useNewUrlParser: true, useUnifiedTopology: true },
@@ -96,6 +94,7 @@ api.delete("/api/pokemons", (request, response) => {
     });
   } else {
     Pokemon.deleteOne({ _id: id }, (error) => {
+      //Ojo en estos casos siempre borrar usando el ID, para estar seguros de lo que borramos.
       if (error) console.error(error);
       else {
         response.send({
@@ -107,104 +106,28 @@ api.delete("/api/pokemons", (request, response) => {
   }
 });
 
-//GET ONE POKEMON. CON QUERY PARAMS.
-api.get("/api/onepokemon", (request, response) => {
-  const id = Number.parseInt(request.query.id);
-  if (!id) {
-    response.status(400).send({
-      success: false,
-      url: "/api/pokemons",
-      method: "GET",
-      message: "id is required",
-    });
-  } else {
-    fs.readFile(DB_POKEMON, (error, data) => {
-      if (error) throw error; //Similar al console.error. Elevar o notificar una excepción.
-      const pokemonJSON = JSON.parse(data); //Data no lo lee como un JSON, sino como texto plano.
-      const onePokemon = pokemonJSON.filter((pokemon) => pokemon.id === id);
-      if (onePokemon.length === 0) {
-        response.status(200).send({
-          success: true,
-          url: "/api/onepokemon",
-          method: "GET",
-          message: "Pokemon not found!",
-        });
-      } else {
-        response.status(200).send({
-          success: true,
-          url: "/api/onepokemon",
-          method: "GET",
-          message: "Pokemon found!",
-          pokemon: onePokemon, //Ojo devuelve un array con un objeto dentro.
-        });
-      }
-    });
-  }
-});
-
-//GET UN POKEMON PERO POR UN MÉTODO MEJOR. Usar url con params más elegantes. Buenas prácticas.
-api.get("/api/pokemons/:id", (request, response) => {
-  console.log(request.params); //Nuestro ID está en la propiedad PARAMS de la REQUEST.
-  let id = Number.parseInt(request.params.id);
-  fs.readFile(DB_POKEMON, (error, data) => {
-    if (error) throw error;
-    let pokemonList = JSON.parse(data);
-    let pokemon = pokemonList.filter((pokemon) => pokemon.id === id);
-    if (pokemon.length === 0) {
-      response.status(200).send({
-        success: true,
-        url: "/api/pokemons",
-        method: "GET",
-        message: "Pokemon not found!",
-      });
-    } else {
-      response.status(200).send({
-        success: true,
-        url: "/api/pokemons",
-        method: "GET",
-        message: "Pokemon found!",
-        pokemon: pokemon, //Ojo devuelve un array con un objeto dentro.
-      });
+//GET Params
+api.get("/api/pokemons/:name", (request, response) => {
+  let name = request.params.name;
+  Pokemon.findOne({ name: name }, (error, data) => {
+    if (error) console.error(error);
+    else {
+      response.send(data);
     }
   });
 });
 
 //PUT
 api.put("/api/pokemons/:id", (request, response) => {
-  let id = Number.parseInt(request.params.id);
-  fs.readFile(DB_POKEMON, (error, data) => {
-    if (error) throw error;
-    let pokeList = JSON.parse(data);
-    let pokeIndex = pokeList.findIndex((pok) => pok.id === id);
-    let pokemonWanted = pokeList.find((pok) => pok.id === id);
-    let modifiedData = request.body;
-    //Se podría intentar hacer con un map para que, si se hace match, que se cambie todo.
-    for (let prop in pokemonWanted) {
-      for (let modProp in modifiedData) {
-        if (modProp === prop) {
-          pokemonWanted[prop] = modifiedData[modProp];
-        }
-      }
+  let id = request.params.id;
+  Pokemon.findByIdAndUpdate(id, { $set: request.body }, (error, data) => {
+    if (error) console.error(error);
+    else {
+      response.status(200).send({
+        success: 200,
+        message: `Pokémon ${request.body.name} was updated successfully`,
+      });
     }
-    pokeList[pokeIndex] = pokemonWanted;
-
-    fs.writeFile(DB_POKEMON, JSON.stringify(pokeList), (error) => {
-      if (error) {
-        response.status(400).send({
-          success: false,
-          url: `/api/pokemons/${id}`,
-          method: "PUT",
-          message: "Can not modify this pokemon!",
-        });
-      } else {
-        response.status(200).send({
-          success: true,
-          url: `/api/pokemons/${id}`,
-          method: "PUT",
-          message: "Pokemon modified!",
-        });
-      }
-    });
   });
 });
 
@@ -259,34 +182,6 @@ api.get("/api/pokemon/pageoffset", (request, response) => {
       method: "GET",
       pokemons: myList,
     });
-  });
-});
-
-//GET LOCATION
-api.get("/api/pokemons/:id/locations/:locationId", (request, response) => {
-  //const { id, locationId } = request.params;
-  const id = Number.parseInt(request.params.id);
-  const locationId = Number.parseInt(request.params.locationId);
-  fs.readFile(DB_POKEMON, (error, data) => {
-    if (error) throw error;
-    let pokeList = JSON.parse(data);
-    let pokemon = pokeList.find((pokemon) => pokemon.id === id);
-    let location = pokemon.locations.find((loc) => loc.id === locationId);
-    if (location) {
-      response.status(200).send({
-        success: true,
-        url: `/api/pokemon/location`,
-        method: "GET",
-        location: location,
-      });
-    } else {
-      response.status(200).send({
-        success: true,
-        url: `/api/pokemon/location`,
-        method: "GET",
-        message: `location not found`,
-      });
-    }
   });
 });
 
